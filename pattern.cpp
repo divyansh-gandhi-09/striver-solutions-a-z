7dbc57a647788c13979be6dd988dd9cbd0d2ecfd
divyansh <divyansh090401@gmail.com>
2024-08-23 00:30:57 +0530

    completing program adding 20-22

--- pattern.cpp
+++ pattern.cpp
@@ -214,9 +214,68 @@ void printpattern19(int n){
         cout<<endl;
    } 
 }
+void printpattern20(int n){
+    for (int i=0;i<2*n-1;i++){
+        if (i<n){
+            for (int a=0;a<=i;a++){
+                cout<<"*";
+            }
+            for (int s=2*n-2;s>2*(i);s--){
+                cout<<" ";
+            }
+            for (int d=0;d<=i;d++){
+                cout<<"*";
+            }
+
+        }
+        else{
+            for (int j=2*n-1;j>i;j--){
+                cout<<"*";
+            }
+            for (int k=0;k<2*(i-n)+2;k++){
+                cout<<" ";
+            }
+            for (int l=2*n-1;l>i;l--){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+
+}
+void printpattern21(int n){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            if (i==0||j==0||i==n-1||j==n-1){
+                cout<<"*";
+            }
+            else {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+void printpattern22(int n){
+    for (int i=0;i<2*n-1;i++){
+        for (int j=0;j<2*n-1;j++){
+            int up=i;
+            int down=(2*n-2)-i;
+            int left=j;
+            int right=2*n-2-j;
+            int min=up;
+            if (down<min) min=down;
+            if (left<min) min=left;
+            if (right<min) min=right;
+            cout<<n-min;
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int n;
     cin>>n;
-    printpattern19(n); 
+    printpattern22(n); 
     
 }
