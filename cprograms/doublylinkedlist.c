ceb917597e3c66054cd82a8e16f18dfc36c78e0c
divyansh <divyansh090401@gmail.com>
2024-10-09 02:51:31 +0530

    test1

--- cprograms/doublylinkedlist.c
+++ cprograms/doublylinkedlist.c
@@ -1,51 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-typedef struct node{
+/*create a doubly inked list having info of employee perform insertion at
+front and deletion at the end*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//creating a struct for doubly ll
+struct node{
     char name[50];
     int emp_id;
-    struct node*start,*forward;
-}node;
-node*create_node(char*name,int emp_id){
-    node *newnode=(node*)malloc(sizeof(node));
-    strcpy(newnode->name, name);
-    newnode->emp_id = emp_id;
-    newnode->forward=NULL;
-    newnode->start=NULL;
-    return newnode;
+    struct node *forward;
+    struct node *backward;
+};
+//function to create a node
+struct node* create_node(int emp_id,char*name){
+    struct node* new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->emp_id=emp_id;
+    strcpy(new_node->name,name);
+    new_node->forward=NULL;
+    new_node->backward=NULL;
+    return new_node;
 }
-void insertion(node**head,char*name,int emp_id,int pos){
-    node*newnode=create_node(name,emp_id);
-    if (pos ==1){
-        newnode->start=NULL;
-        newnode->forward=*head;
+void insertion_ele(struct node**head,int emp_id,char*name){
+    //creating a new node using function
+    struct node* newnode=create_node(emp_id,name);
+    if (*head==NULL){
         *head=newnode;
-        return;
     }
-    node *temp=*head;
-    for (int i = 1; i < pos - 1 && temp != NULL; i++) {
-        temp = temp->forward;
+    else{
+        newnode->forward=*head;
+        (*head)->backward=newnode;
+        *head=newnode;
     }
-    if (temp == NULL) {
-        printf("Position is out of bounds.\n");
-        free(newnode);
-        return;
-    }
-    newnode->start=temp;
-    newnode->forward=temp->forward;
-    temp->forward=newnode;
 }
-void delete_node(node**head){
-    if (*head == NULL) {
-        printf("List is empty.\n");
+void delete_ele(struct node**head){
+    if (*head==NULL){
+        printf("list is empty ");
         return;
     }
-    node* temp = *head;
-    for (int i = 1; temp != NULL; i++) {
-        temp = temp->forward;
+    //temp
+    struct node*temp=*head;
+    while (temp->forward!=NULL){
+        temp=temp->forward;
     }
-    if (temp == NULL || temp->forward == NULL) {
-        printf("Position is out of bounds.\n");
-        return;
+    if (temp->backward!=NULL){
+        temp->backward->forward=NULL;
     }
+    else{
+        *head=NULL;
+    }
+    free(temp);
+}
+void display(struct node**head){
+    struct node*temp1=*head;
+    while(temp1!=NULL){
+        printf("ID:%d,name:%s\n",temp1->emp_id,temp1->name);
+        temp1=temp1->forward;
+    }
+}
+int main(){
+    struct node*head=NULL;
+    insertion_ele(&head,1,"alice");
+    insertion_ele(&head,2,"divyansh");
+    insertion_ele(&head,3,"riya");
+    printf("original list");
+    display(&head);
+    delete_ele(&head);
+    display(&head);
+
+    
 }
