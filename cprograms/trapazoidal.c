5d9309c3f093a51c8e5c42fb6d36581d88bece49
divyansh <divyansh090401@gmail.com>
2024-10-22 13:43:40 +0530

    old

--- cprograms/trapazoidal.c
+++ cprograms/trapazoidal.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
-double function(double x){
+float function(float x){
     return x*x+1;
 }
 //function to calculate the approx integral using trapezoidal rule
-double trapezoidal(double a ,double b,int n){
-    double h=(b-a)/n;
-    double sum=(function(a)+function(b))/2;
+float trapezoidal(float a ,float b,int n){
+    float h=(b-a)/n;
+    float sum=(function(a)+function(b))/2;
     for (int i=1;1<n;i++){
         sum+=function(a+i*h);
     }
-    return sum*h;
+    return (sum*h);
 }
 int main(){
-    double a,b;
+    float a,b;
     int n;
     printf("enter lower limit a:");
-    scanf("%lf",&a);
+    scanf("%f",&a);
     printf("enter upper limit b:");
-    scanf("%lf",&b);
+    scanf("%f",&b);
     printf("Enter the number of intervals n: ");
     scanf("%d", &n);
-    double result =trapezoidal(a,b,n);
-    printf("The approximate value of the integral is: %.6f\n", result);
+    float result =trapezoidal(a,b,n);
+    printf("The approximate value of the integral is: %f\n", result);
     return 0;
 }
